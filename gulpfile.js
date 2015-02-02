21f10708cab0ed27bcb29931adf4a8b5d780731d
Ken Snyder <kendsnyder@gmail.com>
2015-02-02 11:02:57 -0700

    Demo working. gulp build working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,46 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+"use strict";
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var minifyCSS = require('gulp-minify-css');	
+var rename = require('gulp-rename');
+
+/*
+ * Lint js files
  */
+gulp.task('lint', function() {
+	var options = {};
+	return gulp.src('src/jquery.rimodal.js')
+		.pipe(jshint(options));
+});	
 
+/*
+ * Minify js
+ */
+gulp.task('build-js', function() {
+	// Minify and copy src files
+	var options = {
+		preserveComments: 'some'
+	};
+	return gulp.src('src/jquery.rimodal.js')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(uglify(options))
+		.pipe(gulp.dest('.'));
+});
 
+/*
+ * Minify css
+ */
+gulp.task('build-css', function() {
+	// Minify and copy src files
+	return gulp.src('src/jquery.rimodal.css')
+		.pipe(rename({suffix: '.min'}))
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('.'));
+});
+
+/*
+ * Lint and minify
+ */
+gulp.task('build', ['lint', 'build-js', 'build-css']);
